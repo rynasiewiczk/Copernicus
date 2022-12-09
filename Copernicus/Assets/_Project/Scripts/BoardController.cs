9d1915c0d299aa796e3c6a29c85ba8aef8610096
krzysiek <kjanik14@gmail.com>
2022-12-09 23:07:16 +0100

    validate position

--- Copernicus/Assets/_Project/Scripts/BoardController.cs
+++ Copernicus/Assets/_Project/Scripts/BoardController.cs
@@ -1,19 +1,69 @@
 namespace _Project.Scripts
 {
     using System.Collections.Generic;
+    using UnityEngine;
 
     public class BoardController : SingletonBehaviour<BoardController>
     {
+        private readonly List<Vector2Int> _neighborsPositions = new()
+        {
+            Vector2Int.down,
+            Vector2Int.left,
+            Vector2Int.up,
+            Vector2Int.right
+        };
+
         private List<Block> _blocksOnBoard;
 
         public bool IsPositionValidForGroup(Group group)
         {
+            //check if all places empty
+            foreach (var blockInGroup in group.Blocks)
+            {
+                if (TryGetBlockAtPosition(blockInGroup.GetGridPosition(), out var blockOnMap))
+                {
+                    return false; //there is block at this position already
+                }
+            }
+            
+            //check if there is neighbor 
+            foreach (var blockInGroup in group.Blocks)
+            {
+                var blockPosition = blockInGroup.GetGridPosition();
+                foreach (var neighborsPosition in _neighborsPositions)
+                {
+                    var positionToCheck = blockPosition + neighborsPosition;
+                    if (TryGetBlockAtPosition(positionToCheck, out var blockOnMap))
+                    {
+                        return true; //only one neighbor is required
+                    }
+                }
+            }
+            
             return false;
         }
 
         public void PutGroupOnBoard(Group group)
         {
-            //add blocks to board
+            foreach (var blockInGroup in group.Blocks)
+            {
+                _blocksOnBoard.Add(blockInGroup);
+            }
+        }
+
+        private bool TryGetBlockAtPosition(Vector2Int gridPosition, out Block block)
+        {
+            foreach (var blockOnBoard in _blocksOnBoard)
+            {
+                if (blockOnBoard.GetGridPosition() == gridPosition)
+                {
+                    block = blockOnBoard;
+                    return true;
+                }
+            }
+
+            block = null;
+            return false;
         }
     }
 }
